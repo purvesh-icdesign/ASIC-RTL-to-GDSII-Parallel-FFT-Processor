4f102feb0003852fb08fc7ca3f67d1efa4370639
Purvesh Kumar <chandrakarpurvesh@gmail.com>
2026-06-29 17:56:37 +0530

    Add files via upload

---
+++ 4_fft_postSynth_GLS/zero_delay_simulation.f
@@ -0,0 +1,16 @@
+-gui
+
+/home/vlsilab/Music/FFT/FFT/4_fft_postSynth_GLS/zero_delay_simulation/fft_8_top_incremental.v
+/home/vlsilab/Music/FFT/FFT/4_fft_postSynth_GLS/zero_delay_simulation/fft_wps_tb.sv
+/opt/sclpdkv3/scl180/stdcell/fs120/6M1L/verilog/vcs_sim_model/tsl18fs120_scl.v
+/opt/sclpdkv3/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3c01.v
+/opt/sclpdkv3/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3d01.v
+/opt/sclpdkv3/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3o05.v
+ 
+-relax
+-timescale 1ns/10ps 
+-mess 
++delay_mode_zero 
+-access +rwc
+
+
